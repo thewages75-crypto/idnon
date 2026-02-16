603012e4a71529800511f86ecff9a8e8604a1310
thewages75-crypto <thewages75@gmail.com>
2026-02-17 03:45:29 +0530

    Update ADMIN_ID to new Telegram ID

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ from telebot.types import (
 
 API_TOKEN = os.getenv("BOT_TOKEN")
 DATABASE_URL = os.getenv("DATABASE_URL")
-ADMIN_ID = 123456789  # 🔴 Replace with your Telegram ID
+ADMIN_ID = 8409712258  #  Replace with your Telegram ID
 
 bot = telebot.TeleBot(API_TOKEN)
 
